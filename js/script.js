2c2a7c0fd400014f5148562f75f72869319e6c74
Luis Santos <samaisantos@gmail.com>
2014-01-18 16:32:48 -0500

    fixed animation

--- js/script.js
+++ js/script.js
@@ -4,17 +4,22 @@ $(document).ready(function () {
 
 	$('body').keyup(function(e) {
 
+		if(e.keyCode == 37 || e.keyCode == 38 || e.keyCode == 39 || e.keyCode == 40) {
+			$('.version-text').fadeOut(500);	
+		}
+
+
 		clearTimeout(wto);
 		wto = setTimeout(function() {
 			var index;
 			if(e.keyCode == 37 || e.keyCode == 38 || e.keyCode == 39 || e.keyCode == 40) {
 				index = $("#impress div").index($(".present"));		
 			}
-				$('.version-text').fadeOut(100);
+				
 				$('.version-text').text("V" + index);
+				
 				$('.version-text').fadeIn(500);
 		}, 500)
-
 
 	});
 })
